042c30378b3d0ff44e8fe309c8e81600a5fb097f
gauravkaushikcode <gori.kaushik@gmail.com>
2020-11-30 16:22:53 +0200

    first joke app code
    initial commit

---
+++ src/test/java/kaushik/springframework/joke/JokeappApplicationTests.java
@@ -0,0 +1,13 @@
+package kaushik.springframework.joke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
