83b78c0c4755e7730ba611e6023c41dcc843e50d
gauravkaushikcode <gori.kaushik@gmail.com>
2020-11-30 17:06:25 +0200

    Service and View Layer
    service implementation and chuck norris joke actuations

---
+++ src/main/java/kaushik/springframework/joke/controllers/JokeController.java
@@ -0,0 +1,24 @@
+package kaushik.springframework.joke.controllers;
+
+import kaushik.springframework.joke.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+    private JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/",""})
+    public String showJoke(Model model){
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "chucknorris";
+    }
+}
---
+++ src/main/java/kaushik/springframework/joke/services/JokeService.java
@@ -0,0 +1,5 @@
+package kaushik.springframework.joke.services;
+
+public interface JokeService {
+    String getJoke();
+}
