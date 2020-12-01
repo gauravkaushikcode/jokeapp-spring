95c5502656491e3c3ba5b77194a65f7e5a274ba8
gauravkaushikcode <gori.kaushik@gmail.com>
2020-12-01 13:53:10 +0200

    Adding Config
    an example java configuration example

--- src/main/java/kaushik/springframework/joke/services/JokeServiceImpl.java
+++ src/main/java/kaushik/springframework/joke/services/JokeServiceImpl.java
@@ -8,9 +8,12 @@ public class JokeServiceImpl implements JokeService {
 
     private final ChuckNorrisQuotes chuckNorrisQuotes;
 
-    public JokeServiceImpl() {
-        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
     }
+//    public JokeServiceImpl() {
+//        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+//    }
 
     @Override
     public String getJoke() {
